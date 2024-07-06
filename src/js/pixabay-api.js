9091586cbfb5026192b55f245ed1b6bd1a07092c
Sergey Shulga <shul.sergio@gmail.com>
2024-07-06 23:00:28 +0300

    v 1.3 fix
    v 1.3 fix

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,13 @@
+const API_KEY = '36975970-2726c7257b1c2078714098d3d';
+
+export function onGetPhotoByText(query) {
+  return fetch(`https://pixabay.com/api?key=${API_KEY}&q=${query}`).then(
+    res => {
+      console.log('res ', res);
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    }
+  );
+}
--- src/main.js
+++ src/main.js
@@ -6,3 +6,10 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { onGetPhotoByText } from './js/pixabay-api';
+
+const formElements = document.querySelector('.form-input');
+
+onGetPhotoByText('black+car')
+  .then(data => console.log(data))
+  .catch(err => console.error('err', err));
