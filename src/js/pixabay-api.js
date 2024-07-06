7fa812eaf6f41aa3f0c69c4f6777b0a70424408b
Sergey Shulga <shul.sergio@gmail.com>
2024-07-06 23:03:30 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,11 @@
 const API_KEY = '36975970-2726c7257b1c2078714098d3d';
 
 export function onGetPhotoByText(query) {
-  return fetch(`https://pixabay.com/api?key=${API_KEY}&q=${query}`).then(
-    res => {
-      console.log('res ', res);
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      return res.json();
+  return fetch(`http://pixabay.com/api?key=${API_KEY}&q=${query}`).then(res => {
+    console.log('res ', res);
+    if (!res.ok) {
+      throw new Error(res.status);
     }
-  );
+    return res.json();
+  });
 }
