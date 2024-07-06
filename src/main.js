a3ca1a000072bae13000efe7e4ba988bcb873722
Sergey Shulga <shul.sergio@gmail.com>
2024-07-06 20:04:29 +0300

    v 1.2 add input html
    v 1.2 add input html

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
